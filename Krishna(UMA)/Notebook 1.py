ff7a7ef84a0337e8cfada86acf07157f8ceee8b0
k-lakshmikanth <k-lakshmikanth>
2023-11-07 15:58:00 +0000

    Create table

--- Krishna(UMA)/Notebook 1.py
+++ Krishna(UMA)/Notebook 1.py
@@ -1,2 +1,7 @@
 # Databricks notebook source
 spark.sql("SELECT current_timestamp() AS CURRENTSYSTEMDATE").first().CURRENTSYSTEMDATE
+
+# COMMAND ----------
+
+from datetime import datetime
+spark.table(f"{DestinationSchema}.{src_name}_{table_name}").write.json(f"{abfss_path}/{src_name}/in/{LoadType}/{datetime.now().strftime(f'%Y/%m/%d/{table_name}_%m%d%Y_%H%M%S.json')}")
