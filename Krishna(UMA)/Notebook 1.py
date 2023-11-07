f2ab02c6a267ce0b40382bcee678b5bd9ca66bda
k-lakshmikanth <k-lakshmikanth>
2023-11-07 16:13:55 +0000

    a

--- Krishna(UMA)/Notebook 1.py
+++ Krishna(UMA)/Notebook 1.py
@@ -5,3 +5,7 @@ spark.sql("SELECT current_timestamp() AS CURRENTSYSTEMDATE").first().CURRENTSYST
 
 from datetime import datetime
 spark.table(f"{DestinationSchema}.{src_name}_{table_name}").write.json(f"{abfss_path}/{src_name}/in/{LoadType}/{datetime.now().strftime(f'%Y/%m/%d/{table_name}_%m%d%Y_%H%M%S.json')}")
+
+# COMMAND ----------
+
+f"abfss://{container}@{adls_name}.dfs.core.windows.net/"
