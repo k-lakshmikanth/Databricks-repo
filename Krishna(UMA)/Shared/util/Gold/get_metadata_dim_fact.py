6847430ecbe4d7659b4f9f4fda6583bac8a5220c
k-lakshmikanth <k-lakshmikanth>
2023-11-14 12:57:22 +0000

    dev

--- Krishna(UMA)/Shared/util/Gold/get_metadata_dim_fact.py
+++ Krishna(UMA)/Shared/util/Gold/get_metadata_dim_fact.py
@@ -1,13 +1,10 @@
 # Databricks notebook source
-dim_fact_table=spark.sql("select distinct Source_table,Source_Schema,Target_Table,Target_Schema from kpi_etl_analytics_conf.ctl_table_dims_facts order by Target_Table").toJSON().collect()
-
+dim_fact_table = spark.sql("select distinct Source_table,Source_Schema,Target_Table,Target_Schema from kpi_etl_analytics_conf.ctl_table_dims_facts order by Target_Table").toJSON().collect()
 
 # COMMAND ----------
 
 dim_fact_table
 
-
 # COMMAND ----------
-
 
 dbutils.notebook.exit(dim_fact_table)
